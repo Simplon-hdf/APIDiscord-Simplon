8119eeeb250d7a1070f8a65e0a064e071ec98066
NelsonPhilippe <nenelphinel59@gmail.com>
2023-02-28 12:29:14 +0100

    build(sql) : change uuid to big int

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { UsersModule } from './users/users.module';
 import { GuildsModule } from './guilds/guilds.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [UsersModule, GuildsModule],
+  imports: [UsersModule, GuildsModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
