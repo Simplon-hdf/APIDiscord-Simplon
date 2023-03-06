928e9c69232e9304cdcea0febdb8f826609b92ab
Mecopi <mecopilive@gmail.com>
2023-03-06 14:42:30 +0100

    feat(controler): add road to create new promo

--- src/promo/controller/promo.controller.ts
+++ src/promo/controller/promo.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PromoService } from '../service/promo.service';
 import { promo } from '@prisma/client';
+import { RegisterPromoDto } from '../dto/RegisterPromoDTO';
 
 @Controller('promo')
 export class PromoController {
@@ -14,5 +15,10 @@ export class PromoController {
   @Get(':id')
   async getPromo(@Param('id') id: string): Promise<promo> {
     return this.promoService.getPromo(+id);
+  }
+
+  @Post('/create')
+  async createPromo(@Body() data: RegisterPromoDto) {
+    this.promoService.createPromo(data);
   }
 }
