795ef027dbad932ee3d04b5a68c90d635b33a4f7
Mecopi <mecopilive@gmail.com>
2023-03-06 14:43:25 +0100

    feat(service): add func to check promo datas, and new func to register in bdd

--- src/promo/service/promo.service.ts
+++ src/promo/service/promo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
-import { promo } from '@prisma/client';
+import { Prisma, promo } from '@prisma/client';
+import { RegisterPromoDto } from '../dto/RegisterPromoDTO';
 
 @Injectable()
 export class PromoService {
@@ -11,5 +12,49 @@ export class PromoService {
 
   async getPromo(id_to_reach: number): Promise<promo> {
     return this.prisma.promo.findUnique({ where: { id: id_to_reach } });
+  }
+
+  async registerPromo(data: Prisma.promoCreateInput): Promise<promo> {
+    return this.prisma.promo.create({ data });
+  }
+
+  // Func
+
+  async createPromo(dto: RegisterPromoDto) {
+    try {
+      const courseId = await this.prisma.courses.findFirstOrThrow({
+        where: { id: dto.id_courses },
+      });
+
+      const roleId = await this.prisma.roles.findFirstOrThrow({
+        where: { id: dto.id_roles },
+      });
+
+      const newPromo = await this.registerPromo({
+        promo_state: dto.promo_state,
+        code_request: dto.code_request,
+        courses: {
+          connect: {
+            id: +courseId,
+          },
+        },
+        roles: {
+          connect: {
+            id: +roleId,
+          },
+        },
+      });
+
+      return {
+        statusCode: 202,
+        data: newPromo,
+      };
+    } catch (error) {
+      if (error.name != 'NotFoundError') {
+        return error;
+      } else {
+        return "This promo can't be created, check fields";
+      }
+    }
   }
 }
