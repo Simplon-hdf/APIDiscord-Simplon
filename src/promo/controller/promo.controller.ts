ce6f2c2a7cbf203b3c2ab7e920208c8d929d3e67
Mecopi <mecopilive@gmail.com>
2023-03-06 13:55:12 +0100

    feat(common): add getting promos road

--- src/promo/controller/promo.controller.ts
+++ src/promo/controller/promo.controller.ts
@@ -1,19 +1,18 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PromoService } from '../service/promo.service';
 import { promo } from '@prisma/client';
-@Controller('api/v1/promo')
+
+@Controller('promo')
 export class PromoController {
   constructor(private readonly promoService: PromoService) {}
+
   @Get()
-  async getAllTodo(): Promise<promo[]> {
-    return this.promoService.getAllTodo();
+  async getAll(): Promise<promo[]> {
+    return this.promoService.getAll();
   }
-  @Post()
-  async createTodo(@Body() postData: promo): Promise<promo> {
-    return this.promoService.createTodo(postData);
-  }
+
   @Get(':id')
-  async getTodo(@Param('id') id: number): Promise<promo | null> {
-    return this.promoService.getTodo(id);
+  async getPromo(@Param('id') id: string): Promise<promo> {
+    return this.promoService.getPromo(+id);
   }
 }
