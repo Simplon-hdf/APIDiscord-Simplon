2feada8e7debf16dae1bc122047380a972a70102
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-03-09 16:20:02 +0100

    feat(signature): create a CRON for code Request status reset every day at 13.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,22 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { UsersModule } from './users/users.module';
 import { SignatureModule } from './signature/signature.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TasksService } from './tasks.service';
+import { SignatureService } from './signature/signature.service';
+import { UsersService } from './users/users.service';
+import { GuildService } from './guild/guild.service';
 
 @Module({
-  imports: [UsersModule, SignatureModule],
+  imports: [UsersModule, SignatureModule, ScheduleModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService,
+    PrismaService,
+    TasksService,
+    SignatureService,
+    UsersService,
+    GuildService,
+  ],
 })
 export class AppModule {}
---
+++ src/tasks.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { SignatureService } from './signature/signature.service';
+import { PrismaService } from './prisma.service';
+
+@Injectable()
+export class TasksService {
+  constructor(
+    private signature: SignatureService,
+    private prisma: PrismaService,
+  ) {}
+  private readonly logger = new Logger(TasksService.name);
+
+  @Cron(CronExpression.EVERY_30_SECONDS /*EVERY_DAY_AT_1AM*/)
+  async handleCron() {
+    console.log('change');
+    await this.prisma.promo.updateMany({
+      data: { code_request: true },
+    });
+  }
+}
